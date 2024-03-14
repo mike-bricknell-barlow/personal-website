49c66e50bc7eb7ddcd26f640983edba4cefd9aad
Mike Bricknell-Barlow <mike@bricknellbarlow.co.uk>
2024-03-14 15:10:43 +0000

    Disabling all but minis section for now

--- index.go
+++ index.go
@@ -15,7 +15,10 @@ func main() {
     r.PathPrefix("/public/").Handler(http.StripPrefix("/public/", http.FileServer(http.Dir("build/"))))
     r.PathPrefix("/cms/images/").Handler(http.StripPrefix("/cms/images/", http.FileServer(http.Dir("cms/public/"))))
 
-    r.HandleFunc("/", controllers.HomeHandler)
+    //r.HandleFunc("/", controllers.HomeHandler)
+    r.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+        http.Redirect(w, r, "/miniatures/", http.StatusSeeOther)
+    })
 
     r.HandleFunc("/miniatures/", controllers.MinisHandler)
     r.HandleFunc("/miniatures/page/{pageNum}/", controllers.MinisHandler)
